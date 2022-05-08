f2c793d82c7f3c885016f35c1a4b6660f5f5c384
Carolina Santiago <ctt.carolinasantiago@gmail.com>
2022-05-08 16:29:07 -0300

    Create input-unction-ex.py

---
+++ input-unction-ex.py
@@ -0,0 +1,11 @@
+# Instruções
+# Ler um número inteiro.
+# Imprimir o número lido multiplicado por 2.
+# Exemplos:
+# Input: 1 Output: 2
+# Input: 0 Output: 0
+# Input: 10 Output: 20
+# Input: -3 Output: -6
+
+n = int(input())
+print(2*n)
